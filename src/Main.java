07661d61dd9b8021bd6f75bed08ea0074ac8c971
Bortokp <bortokp@gmail.com>
2022-05-24 22:36:23 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args)
     {
-        SymbolTable dupa = new SymbolTable();
-        dupa.getHashList().get(0).put("string1","string");
-        dupa.getHashList().get(0).put("string2","string");
-        dupa.getHashList().get(0).put("string3","string");
-        dupa.getHashList().get(0).put("string4","string");
-        dupa.printTable();
+        SymbolTable tablica = new SymbolTable();
+        tablica.getHashList().get(0).put("string1","string");
+        tablica.getHashList().get(0).put("string2","string");
+        tablica.getHashList().get(0).put("string3","string");
+        tablica.getHashList().get(0).put("string4","string");
+        tablica.printTable();
     }
 }
